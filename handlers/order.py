051add38eca5ecd773d4a5200a73ec385e582fda
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-02-13 17:18:11 +0500

    Version 3.27
    Realise version
    
    Cosmetical fix

--- handlers/order.py
+++ handlers/order.py
@@ -240,7 +240,7 @@ async def taxi_tax(message:Message):
 			else:
 				await message.answer(f'На вашем балансе недостаточно средств\nСтоимость одной заявки: {parameters["count"]} руб.\nНа вашем балансе: {driver_info[1]["balance"]} руб.', keyboard=keyboards.inline.payments)
 		else:
-			await message.answer(f'Пассажир отменил вызов или эту заявку уже принял другой водитель.', keyboard=keyboards.driver_registartion_success)
+			await message.answer(f'Пассажир отменил вызов, или эту заявку уже принял другой водитель.', keyboard=keyboards.driver_registartion_success)
 
 # Принимаем доставку
 @vk.on.private_message(Delivery())
@@ -334,7 +334,7 @@ async def driver_delivery(message:Message):
 			else:
 				await message.answer(f'На вашем балансе недостаточно средств\nСтоимость одной заявки: {parameters["count"]} руб.\nНа вашем балансе: {driver_info[1]["balance"]} руб.', keyboard=keyboards.inline.payments)
 		else:
-			await message.answer(f'Пассажир отменил вызов или эту заявку уже принял другой водитель.', keyboard=keyboards.driver_registartion_success)
+			await message.answer(f'Пассажир отменил вызов, или эту заявку уже принял другой водитель.', keyboard=keyboards.driver_registartion_success)
 
 @vk.on.private_message(state = DeliveryState.location)
 async def delivery_tax(message:Message):
