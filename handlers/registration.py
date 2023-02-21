17ab5d503a8da4fc060fe1474ef15690990b81b6
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-02-21 21:10:29 +0500

    Version 3.43
    Realise version
    
    Cosmetical fix

--- handlers/registration.py
+++ handlers/registration.py
@@ -13,16 +13,17 @@ async def registartion_driver(message:Message):
 	parameters = await binder.get_parameters()
 	await dispatcher.update_no_registred_driver(message.from_id)
 	storage.set(f'{message.from_id}_balance', 0)
-	await vk.state_dispenser.set(message.from_id, DriverRegState.location)
-	await message.answer(f'Регистрация!\nТекущий город: {parameters["city"]}\n\nЕсли вы из другого города, то напишите город или отправьте геолокацию', keyboard = keyboards.inline.pass_this_step)
+	storage.set(f'{message.from_id}_location', f'{parameters["city"]}')
+	await vk.state_dispenser.set(message.from_id, DriverRegState.phone)
+	await message.answer('Отправьте ваш телефон для связи с пассажиром!\n\nТелефон можно не указывать, однако тогда пассажир не сможет связаться с Вами, когда вы подъедете к месту вызова!', keyboard=keyboards.inline.phone_pass_this_step)
 
 # Редактирование профиля пассажира
-@vk.on.private_message(payload = {'user': 0, 'edit': 0})
-async def passanger_edit_profile(message:Message):
-	await db.passanger.delete(message.from_id)
-	await vk.state_dispenser.set(message.from_id, PassangerRegState.phone)
-	await message.answer('Отправьте ваш телефон для связи с водителем!\n\n\
-Телефон можно не указывать, однако тогда водитель не сможет связаться с Вами, когда подъедет к месту вызова!', keyboard=keyboards.inline.phone_pass_this_step)
+# @vk.on.private_message(payload = {'user': 0, 'edit': 0})
+# async def passanger_edit_profile(message:Message):
+# 	await db.passanger.delete(message.from_id)
+# 	await vk.state_dispenser.set(message.from_id, PassangerRegState.promo)
+# 	await message.answer('Отправьте ваш телефон для связи с водителем!\n\n\
+# Телефон можно не указывать, однако тогда водитель не сможет связаться с Вами, когда подъедет к месту вызова!', keyboard=keyboards.inline.phone_pass_this_step)
 	
 @vk.on.private_message(state = PassangerRegState.phone)
 async def reg_passanger_2(message:Message):
@@ -31,23 +32,46 @@ async def reg_passanger_2(message:Message):
 		storage.set(f'phone_{message.from_id}', f'@id{message.from_id}')
 	else:
 		storage.set(f'phone_{message.from_id}', message.text)
-	await vk.state_dispenser.set(message.from_id, PassangerRegState.location)
-	await message.answer(f'Текущий город: {parameters["city"]}\n\nЕсли вы хотите сменить город то напишите его название или пришлите геолокацию', keyboard = keyboards.inline.pass_this_step)
+	if (await vk.state_dispenser.get(message.from_id)) is not None:
+		await vk.state_dispenser.delete(message.from_id)
+	location = storage.get(f'{message.from_id}_location')
+	phone = storage.get(f'phone_{message.from_id}')
+	storage.delete(f'phone_{message.from_id}'); storage.delete(f'{message.from_id}_location')
+	user_data = await vk.api.users.get(
+		user_ids = message.from_id,
+		fields = 'sex'
+	)
+	await message.answer('Ваша анкета успешно создана!', keyboard = keyboards.choose_service)
+	await db.passanger.reg({ # Непосредственно регистрация
+		'vk': message.from_id,
+		'gender': str(user_data[0].sex)[8:],
+		'city': location,
+		'name': user_data[0].first_name,
+		'phone': phone
+	})
+	await asyncio.sleep(2)
+	await message.answer('Ты можешь получить бонусные рубли на баланс анкеты, и обменять их на бесплатные поездки по городу.\n\n\
+Для этого нужно переслать пригласительное сообщение друзьям. За каждую регистрацию с использованием твоего промокода ты получишь по 10р. и сможешь обменять их на поездки по городу.\n\n\
+Разошли пригласительное сообщение своим друзьям &#128071;&#128071;&#128071;')
+	await asyncio.sleep(1)
+	promo = await dispatcher.add_new_promo(message.from_id)
+	id_ = await vk.api.groups.get_by_id()
+	await message.answer(f'Смотри каким ботом такси я пользуюсь: https://vk.com/write-{id_[0].id}\n\
+Создай свою анкету используя мой промокод "{promo[1]}" и получи бесплатные поездки по городу!')
 
-@vk.on.private_message(state = PassangerRegState.location)
-async def reg_passanger_3(message:Message):
-	parameters = await binder.get_parameters()
-	if message.geo is not None: # Если геолокация отправлена
-		storage.set(f'{message.from_id}_location', message.geo.place.city)
-	elif message.text.lower() == 'пропустить шаг':
-		storage.set(f'{message.from_id}_location', f'{parameters["city"]}')
-	else:
-		if (await ddt.suggest('address', message.text)) != []:
-			storage.set(f'{message.from_id}_location', message.text.lower())
-		else:
-			return 'Возможно вы неверно написали город!\nПопробуйте снова'
-	await vk.state_dispenser.set(message.from_id, PassangerRegState.promo)
-	await message.answer('Введите пригласительный реферальный код.\nЕсли его нет - пропустите', keyboard=keyboards.inline.phone_pass_this_step)
+# @vk.on.private_message(state = PassangerRegState.location)
+# async def reg_passanger_3(message:Message):
+# 	parameters = await binder.get_parameters()
+# 	if message.geo is not None: # Если геолокация отправлена
+# 		storage.set(f'{message.from_id}_location', message.geo.place.city)
+# 	elif message.text.lower() == 'пропустить шаг':
+		
+# 	else:
+# 		if (await ddt.suggest('address', message.text)) != []:
+# 			storage.set(f'{message.from_id}_location', message.text.lower())
+# 		else:
+# 			return 'Возможно вы неверно написали город!\nПопробуйте снова'
+# 	await vk.state_dispenser.set(message.from_id, PassangerRegState.promo)
 
 @vk.on.private_message(state = PassangerRegState.promo)
 async def insert_promo(message:Message):
@@ -71,51 +95,23 @@ async def insert_promo(message:Message):
 					await dispatcher.add_insert_promo_user(message.from_id)
 		else:
 			return 'Такого промокода нет. Попробуйте снова или пропустите шаг!'
-	if (await vk.state_dispenser.get(message.from_id)) is not None:
-		await vk.state_dispenser.delete(message.from_id)
-	location = storage.get(f'{message.from_id}_location')
-	phone = storage.get(f'phone_{message.from_id}')
-	storage.delete(f'phone_{message.from_id}'); storage.delete(f'{message.from_id}_location')
-	user_data = await vk.api.users.get(
-		user_ids = message.from_id,
-		fields = 'sex'
-	)
-	await message.answer('Ваша анкета успешно создана!', keyboard = keyboards.choose_service)
-	await db.passanger.reg({ # Непосредственно регистрация
-		'vk': message.from_id,
-		'gender': str(user_data[0].sex)[8:],
-		'city': location,
-		'name': user_data[0].first_name,
-		'phone': phone
-	})
-	await asyncio.sleep(2)
-	await message.answer('Ты можешь получить бонусные рубли на баланс анкеты, и обменять их на бесплатные поездки по городу.\n\
-\n\
-Для этого нужно переслать пригласительное сообщение друзьям. За каждую регистрацию с использованием твоего промокода ты получишь по 10р. и сможешь обменять их на поездки по городу.\n\
-\n\
-Разошли пригласительное сообщение своим друзьям &#128071;&#128071;&#128071;')
-	await asyncio.sleep(1)
-	promo = await dispatcher.add_new_promo(message.from_id)
-	id_ = await vk.api.groups.get_by_id()
-	await message.answer(f'Смотри каким ботом такси я пользуюсь: https://vk.com/write-{id_[0].id}\n\
-Создай свою анкету используя мой промокод "{promo[1]}" и получи бесплатные поездки по городу!')
+	await vk.state_dispenser.set(message.from_id, PassangerRegState.phone)
+	await message.answer('Отправьте ваш телефон для связи с водителем!\n\nТелефон можно не указывать, однако тогда водитель не сможет связаться с Вами, когда подъедет к месту вызова!', keyboard=keyboards.inline.phone_pass_this_step)
 
 # Регистрация водителя (шаг 1) 
-@vk.on.private_message(state = DriverRegState.location)
-async def reg_driver_loc(message:Message):
-	parameters = await binder.get_parameters()
-	if message.geo is not None:
-		location = message.geo.place.city
-	elif message.text.lower() == 'пропустить шаг':
-		location = f'{parameters["city"]}'
-	else:
-		if (await ddt.suggest('address', message.text)) != []:
-			location = message.text
-		else:
-			return 'Город возможно написан неверно. Попробуйте снова'
-	storage.set(f'location_{message.from_id}', location)
-	await vk.state_dispenser.set(message.from_id, DriverRegState.phone)
-	await message.answer('Отправьте ваш телефон для связи с пассажиром!\n\nТелефон можно не указывать, однако тогда пассажир не сможет связаться с Вами, когда вы подъедете к месту вызова!', keyboard=keyboards.inline.phone_pass_this_step)
+# @vk.on.private_message(state=DriverRegState.location)
+# async def reg_driver_loc(message:Message):
+# 	parameters = await binder.get_parameters()
+# 	if message.geo is not None:
+# 		location = message.geo.place.city
+# 	elif message.text.lower() == 'пропустить шаг':
+# 		location = f'{parameters["city"]}'
+# 	else:
+# 		if (await ddt.suggest('address', message.text)) != []:
+# 			location = message.text
+# 		else:
+# 			return 'Город возможно написан неверно. Попробуйте снова'
+# 	storage.set(f'location_{message.from_id}', location)
 
 # Регистрация водителя (шаг 2)
 @vk.on.private_message(state = DriverRegState.phone)
@@ -209,17 +205,18 @@ async def reg_driver_6(message:Message):
 	await message.answer(f'Смотри каким ботом такси я пользуюсь: https://vk.com/write-{id_[0].id}\n\
 Создай свою анкету используя мой промокод "{promo[1]}" и получи бесплатные поездки по городу!')
 
-# Редактирование (пперерегистрация водителя)
-@vk.on.private_message(payload = {'driver': 0, 'edit': 0})
-async def driver_edit_profile(message:Message):
-	driver_profile = await db.driver.get(message.from_id)
-	storage.set(f'{message.from_id}_balance', driver_profile[1]['balance'])
-	await db.driver.delete(message.from_id)
-	await vk.state_dispenser.set(message.from_id, DriverRegState.location)
-	await message.answer('Редактирование!\n\nТекущий город: Няндома\nЕсли вы из другого города, то отправьте название вашего города или пришлите геолокацию', keyboard = keyboards.inline.pass_this_step)
+# # Редактирование (пперерегистрация водителя)
+# @vk.on.private_message(payload = {'driver': 0, 'edit': 0})
+# async def driver_edit_profile(message:Message):
+# 	driver_profile = await db.driver.get(message.from_id)
+# 	storage.set(f'{message.from_id}_balance', driver_profile[1]['balance'])
+# 	await db.driver.delete(message.from_id)
+# 	await vk.state_dispenser.set(message.from_id, DriverRegState.location)
+# 	await message.answer('Редактирование!\n\nТекущий город: Няндома\nЕсли вы из другого города, то отправьте название вашего города или пришлите геолокацию', keyboard = keyboards.inline.pass_this_step)
 
 # Если человек регестрируется как пассажир (шаг 1)
 @vk.on.private_message(payload = {'passanger': 1})
 async def reg_passanger_1(message:Message):
-	await vk.state_dispenser.set(message.from_id, PassangerRegState.phone)
-	await message.answer('Отправьте ваш телефон для связи с водителем!\n\nТелефон можно не указывать, однако тогда водитель не сможет связаться с Вами, когда подъедет к месту вызова!', keyboard=keyboards.inline.phone_pass_this_step)
+	storage.set(f'{message.from_id}_location', f'{parameters["city"]}')
+	await vk.state_dispenser.set(message.from_id, PassangerRegState.promo)
+	await message.answer('Введите пригласительный реферальный код.\nЕсли его нет - пропустите', keyboard=keyboards.inline.phone_pass_this_step)
