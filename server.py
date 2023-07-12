bd170b25482050deb6b6f0760abbe5bbf313670d
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-07-12 11:19:35 +0500

    Version 3.63

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from aiohttp.web import Application, RouteTableDef, Response, run_app
+from aiohttp.web import Application, RouteTableDef, Response, run_app, Request
 from aiofiles import open as aiopen
 from plugins.database import Database
 
@@ -7,7 +7,7 @@ routes = RouteTableDef()
 db = Database()
 
 @routes.get('/styles/style.css')
-async def server_get_style(request):
+async def server_get_style(request:Request):
 	async with aiopen('table/styles/style.css') as css:
 		css_text = await css.read()
 	return Response(
@@ -16,7 +16,7 @@ async def server_get_style(request):
 	)
 
 @routes.get('/styles/reset.css')
-async def server_get_reset(request):
+async def server_get_reset(request:Request):
 	async with aiopen('table/styles/reset.css') as css:
 		css_text = await css.read()
 	return Response(
@@ -25,7 +25,7 @@ async def server_get_reset(request):
 	)
 
 @routes.get('/styles/style.css.map')
-async def server_get_map(request):
+async def server_get_map(request:Request):
 	async with aiopen('table/styles/style.css.map') as map_:
 		map_text = await map_.read()
 	return Response(
@@ -34,7 +34,7 @@ async def server_get_map(request):
 	)
 
 @routes.get('/drivers')
-async def get_drivers(request):
+async def get_drivers(request:Request):
 	async with aiopen('table/drivers.html') as html:
 		html_text = await html.read()
 	all_drivers = await db.driver.admin_get_all()
@@ -64,7 +64,7 @@ async def get_drivers(request):
 	)
 
 @routes.get('/passangers')
-async def get_passangers(request):
+async def get_passangers(request:Request):
 	async with aiopen('table/passangers.html') as html:
 		html_text = await html.read()
 	all_drivers = [record async for record in db.passanger.admin_get_all()]
@@ -75,13 +75,22 @@ async def get_passangers(request):
 					{name}
 					{phone}
 					{quantity}
-				</tr>''' * (len(all_drivers)))
+					{balance}
+				</tr>''' * len(all_drivers))
 	counter = 0
 	for driver in all_drivers:
-		for index in ['VK', 'gender', 'city', 'name', 'phone', 'quantity']:
+		for index in ['VK', 'gender', 'city', 'name', 'phone', 'quantity', 'balance']:
 			html_text = html_text.replace(f'{{{index}}}', f'<td class="{"tg-pvec" if counter % 2 == 0 else "tg-ku2z"}">{driver[index]}</td>', 1)
 		counter += 1
 	return Response(
 		text=html_text,
 		content_type='text/html'
 	)
+
+@routes.get('/orders')
+async def get_orders(request:Request):
+	pass
+
+if __name__ == '__main__':
+	app.add_routes(routes)
+	run_app(app, host='127.0.0.1', port=80)
