8814bb342039cc46d753374bb0c581a697182808
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-07-18 10:27:16 +0500

    Version 3.67.1: TEST
    That's test branch

--- cplug/tomloader.cpp
+++ cplug/tomloader.cpp
@@ -1,1 +1,28 @@
 # include <Python.h>
+# include "tomloader.hpp"
+
+static PyObject* toml_protect(PyObject *self, PyObject *args) {
+    PyObject* filename_obj;
+
+    if (!PyArg_ParseTuple(args, "U", &filename_obj)) {
+        return NULL;
+    }
+
+    const char* filename = PyUnicode_AsUTF8(filename_obj);
+    toml_protector(filename);
+
+    return PyNone;
+}
+
+static PyObject* toml_is_correct(PyObject *self, PyObject *args) {
+    PyObject* filename_obj;
+
+    if (!PyArg_ParseTuple(args, "U", &filename_obj)) {
+        return NULL;
+    }
+
+    const char* filename = PyUnicode_AsUTF8(filename_obj);
+    toml_protector(filename);
+
+    return PyNone;
+}
